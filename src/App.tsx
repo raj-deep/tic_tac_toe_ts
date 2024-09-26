718d8e21a9c790d927288a7de90adf0f372664f9
raj-deep <deepraj181200@gmail.com>
2024-09-26 08:35:39 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-blue-500 hover:bg-blue-700 border-2 border-dashed border-black flex justify-center h-[30vh] items-center">Hello</h1>
+    </>
+  )
+}
+
+export default App
